49c789102734dc20392079ef658f2a1da187a530
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2019-11-07 11:52:27 -0800

    completed storing location data to SQL

--- schema.sql
+++ schema.sql
@@ -5,14 +5,14 @@ CREATE TABLE locations (
   id SERIAL PRIMARY KEY,
   search_query VARCHAR(255),
   formatted_query VARCHAR(255),
-  latitude NUMERIC(10,8),
+  latitude NUMERIC(10,7),
   longitude NUMERIC(11,8)
 );
 
-CREATE TABLE weather (
-  id SERIAL PRIMARY KEY,
-  forecast VARCHAR(255),
-  time VARCHAR(255),
-  location_id INTEGER NOT NULL
-);
+-- CREATE TABLE weather (
+--   id SERIAL PRIMARY KEY,
+--   forecast VARCHAR(255),
+--   time VARCHAR(255),
+--   location_id INTEGER NOT NULL
+-- );
 
--- server.js
+++ server.js
@@ -15,9 +15,6 @@ const app = express();
 app.use(cors());
 
 const client = new pg.Client(process.env.DATABASE_URL);
-
-client.connect();
-
 client.on('error', err => console.error(err));
 
 // Routes
@@ -27,47 +24,76 @@ app.get('/weather', getWeather);
 
 // Helper Function
 
-function lookup(data) {
-  let SQL = `SELECT * FROM ${data.tableName} FROM location_id=$1`;
+let lookup = (data) => {
+  let SQL = `SELECT * FROM ${data.tableName} WHERE location_id=$1;`;
 
-  client.query(SQL , [data.location_id])
+  return client.query(SQL, [data.location_id])
     .then(results => {
-      results.status(200).json(results);
+      if (results.rowCount > 0) {
+        data.cacheHit(results);
+      } else {
+        data.cacheMiss();
+      }
     })
-    .catch(error => console.error(error));
-}
+    .catch(() => errorHandler());
+};
 
 function homePage(request,response) {
   response.status(200).send('Welcome to the Home Page!');
 }
 
 // Location Methods
-Location.prototype.getLocation(request,response) {
+function getLocation (request,response) {
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
 
   superagent.get(url)
-    .then( data => {
-      const geoData = data.body;
+    .then( result => {
+      const geoData = result.body;
       const location = new Location(request.query.data, geoData);
-      response.status(200).send(location);
+      insert(location);
+      response.send(location);
+      response.send(result);
     })
     .catch( error => {
+      console.error(error);
       response.status(500).send('Status: 500. Sorry, there is something not quite right');
     })
 }
 
+function insert(location) {
+  let SQL = 'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES($1, $2, $3, $4)';
+
+  let safeValues = Object.values(location);
+
+  client.query(SQL, safeValues).catch( error => errorHandler(error));
+}
+
 // Weather Methods
-Weather.prototype.getWeather(request, response) {
+function getWeather (request, response) {
   const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
 
   superagent.get(url)
     .then( data => {
-      const weatherSummaries = data.body.daily.data.map(day => new Weather(day));
-      response.status(200).send(weatherSummaries);
+      const weatherSummaries = data.body.daily.data.map(day => {
+        const weatherDay = new Weather(day);
+        weatherDay.insert(request.query.data.id);
+        return weatherDay
+      });
+      response.json(weatherSummaries);
     })
     .catch( error => {
       errorHandler('So sorry, something went really wrong', request, response);
     });
+}
+
+Weather.prototype.insert = function (id) {
+  let SQL = `INSERT INTO weather (forecast, time, location_id)
+              VALUES($1, $2, $3);`;
+
+  let values = Object.values(this);
+  values.push(id);
+
+  return client.query(SQL, values);
 }
 
 // Weather Constructor Function
@@ -97,4 +123,8 @@ function errorHandler(error,request,response) {
 app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
 
 // PORT to for the server to listen too
-app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
+client.connect()
+  .then ( () => {
+    app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
+  })
+

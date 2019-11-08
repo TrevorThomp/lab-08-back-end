ef8585ca6af0d79c5796aeaad92b38af1f222242
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2019-11-08 09:27:57 -0800

    completed data being stored in DB

--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE locations (
   search_query VARCHAR(255),
   formatted_query VARCHAR(255),
   latitude NUMERIC(10,7),
-  longitude NUMERIC(11,8)
+  longitude NUMERIC(10,7)
 );
 
 CREATE TABLE weather (
